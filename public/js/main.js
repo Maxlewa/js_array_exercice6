da54a04062b19d880f1d478b1d641591f43cb5dd
Maxence <lemairemaxence@hotmail.com>
2021-01-04 09:53:33 +0100

    exo ok sauf point 2

---
+++ public/js/main.js
@@ -0,0 +1,63 @@
+// 1
+
+let monTab = ['pomme', 'fraise', 'melon', 'citron']
+
+// supprimer le dernier élément
+
+// monTab.pop();
+// console.log(monTab);
+
+// supprimer le premier élément
+
+// monTab.shift();
+// console.log(monTab);
+
+// ajouter un élément à la fin de l'array
+
+// monTab.push('pastèque')
+// console.log(monTab);
+
+// ajouter un élément au début de l'array
+
+// monTab.unshift('banane')
+// console.log(monTab);
+
+// deuxième élément en majuscules
+
+// secondElement = monTab[1].toUpperCase();
+// console.log(secondElement);
+
+// changer l'ordre de l'array
+
+// monTab.sort();
+// console.log(monTab);
+
+// 2
+
+let chiffres = [4, 5, 2, 1, 3]
+
+// ordonner les chiffres
+
+chiffres.sort();
+console.log(chiffres);
+
+// concatenation
+
+// 3
+
+// var valentin = []
+
+// valentin.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+// console.log(valentin);
+
+// console.log(valentin.length);
+
+// console.log(valentin.indexOf(3));
+
+// let valentin = ['objet 1', 'objet 2', 'objet 3', 'objet 4', 'objet 5']
+// let index = valentin.indexOf('objet 3');
+// valentin.splice(index, 1);
+// console.log(valentin);
+
+
+
